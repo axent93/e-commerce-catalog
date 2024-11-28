da5d49fb4fe132006fd40ccab0fecb957f2121ae
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2024-11-28 09:21:37 +0100

    feat: added linters, jest tests, prettier, comitlint, husky, json-server and modified project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,53 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
+import prettier from 'eslint-plugin-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig // Ensure Prettier disables conflicting ESLint rules
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier // Add Prettier plugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/prop-types': 'off',
+      'no-debugger': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'none',
+          tabWidth: 2,
+          semi: false,
+          quoteProps: 'as-needed',
+          jsxSingleQuote: true,
+          bracketSpacing: true,
+          bracketSameLine: false,
+          arrowParens: 'avoid',
+          parser: 'typescript',
+          htmlWhitespaceSensitivity: 'css',
+          endOfLine: 'auto',
+          singleAttributePerLine: true,
+          printWidth: 130
+        }
+      ] // Enforce Prettier rules as ESLint errors
+    }
+  }
 )
